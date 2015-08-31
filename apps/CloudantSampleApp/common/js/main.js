ff72ce2983527db5ddd0968d7f7f6f5ec7ca0a55
Nathan Hazout <nathanh@il.ibm.com>
2015-08-31 16:10:00 +0300

    Javascript adapter and hybrid code

--- apps/CloudantSampleApp/common/js/main.js
+++ apps/CloudantSampleApp/common/js/main.js
@@ -1,16 +1,72 @@
 function wlCommonInit(){
-	/*
-	 * Use of WL.Client.connect() API before any connectivity to a MobileFirst Server is required. 
-	 * This API should be called only once, before any other WL.Client methods that communicate with the MobileFirst Server.
-	 * Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
-	 *    
-	 *    WL.Client.connect({
-	 *    		onSuccess: onConnectSuccess,
-	 *    		onFailure: onConnectFailure
-	 *    });
-	 *     
-	 */
-	
-	// Common initialization code goes here
+	getList();
 	
 }
+var busyIndicator = new WL.BusyIndicator( null, {text : 'Loading...'});
+var list = [];
+
+function getList(){
+	busyIndicator.show();
+	var resourceRequest = new WLResourceRequest("/adapters/CloudantJS/getAllEntries", WLResourceRequest.GET, 30000);
+	resourceRequest.send().then(
+		function(results){
+			list = results.responseJSON.rows;
+			displayList();
+		},
+		function(results){
+			alert("error in getList");
+		}
+	);
+}
+
+function displayList(){
+	$('#list').empty();
+	jQuery.each(list, function(index, value){
+		var row = $('<tr>');
+		row.append($('<td>').text(value.name));
+		row.append($('<td>').text(value.age));
+		
+		var del = $('<button>').addClass('delete').text('Delete');
+		del.data('document',value);
+		row.append($('<td>').append(del));
+		
+		$('#list').append(row);
+	});
+	busyIndicator.hide();
+}
+
+$('#refresh').on('click',function(){
+	getList();
+});
+
+$('#add').on('click',function(){
+	busyIndicator.show();
+	var entry = {'name': $('#name').val(), 'age': $('#age').val()};
+	
+	var resourceRequest = new WLResourceRequest("/adapters/CloudantJS/addEntry", WLResourceRequest.POST, 30000);
+	resourceRequest.setQueryParameter("params", "['" + JSON.stringify(entry) + "']");
+	resourceRequest.send().then(
+		function(results){
+			$('#name').val('');
+			$('#age').val('');
+			getList();
+		},
+		function(results){
+			alert("error in add");
+		}
+	);
+});
+
+$('#list').on('click','button.delete',function(){
+	busyIndicator.show();
+	var resourceRequest = new WLResourceRequest("/adapters/CloudantJS/deleteEntry", WLResourceRequest.POST, 30000);
+	resourceRequest.setQueryParameter("params", "['" + JSON.stringify($(this).data().document) + "']");
+	resourceRequest.send().then(
+		function(results){
+			getList();
+		},
+		function(results){
+			alert("error in delete");
+		}
+	);
+});
