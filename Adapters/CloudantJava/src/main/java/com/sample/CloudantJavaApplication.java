00d06ee47e2c3c971ff6526a3eba5603291dce13
Lior Burg <liorbu@il.ibm.com>
2016-01-21 11:46:45 +0200

    Changed the Java Adapter to make it work in 8.0 and using the Cloudant-Client 2.2.0

--- Adapters/CloudantJava/src/main/java/com/sample/CloudantJavaApplication.java
+++ Adapters/CloudantJava/src/main/java/com/sample/CloudantJavaApplication.java
@@ -16,26 +16,23 @@
 
 package com.sample;
 
+import com.ibm.mfp.adapter.api.MFPJAXRSApplication;
+
 import java.util.logging.Logger;
-import com.worklight.wink.extensions.MFPJAXRSApplication;
 
 public class CloudantJavaApplication extends MFPJAXRSApplication{
 
 	static Logger logger = Logger.getLogger(CloudantJavaApplication.class.getName());
 
-	@Override
 	protected void init() throws Exception {
-		CloudantJavaResource.init();
-
 		logger.info("Adapter initialized!");
 	}
 
-	@Override
+
 	protected void destroy() throws Exception {
 		logger.info("Adapter destroyed!");
 	}
 
-	@Override
 	protected String getPackageToScan() {
 		//The package of this class will be scanned (recursively) to find JAX-RS resources.
 		//It is also possible to override "getPackagesToScan" method in order to return more than one package for scanning
--- Adapters/CloudantJava/src/main/java/com/sample/CloudantJavaResource.java
+++ Adapters/CloudantJava/src/main/java/com/sample/CloudantJavaResource.java
@@ -15,6 +15,7 @@
 */
 package com.sample;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -25,51 +26,76 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.lightcouch.NoDocumentException;
-
+import com.cloudant.client.api.ClientBuilder;
 import com.cloudant.client.api.CloudantClient;
 import com.cloudant.client.api.Database;
-import com.worklight.adapters.rest.api.WLServerAPI;
-import com.worklight.adapters.rest.api.WLServerAPIProvider;
-import com.worklight.server.bundle.api.WorklightConfiguration;
+import com.cloudant.client.org.lightcouch.NoDocumentException;
+import com.ibm.mfp.adapter.api.ConfigurationAPI;
+import com.ibm.mfp.adapter.api.OAuthSecurity;
+
 
 @Path("/")
 public class CloudantJavaResource {
 	/*
-	 * For more info on JAX-RS see https://jsr311.java.net/nonav/releases/1.1/index.html
+	 * For more info on JAX-RS see https://jax-rs-spec.java.net/nonav/2.0-rev-a/apidocs/index.html
 	 */
 
 	//Define logger (Standard java.util.Logger)
 	static Logger logger = Logger.getLogger(CloudantJavaResource.class.getName());
 
-    //Define the server api to be able to perform server operations
-    WLServerAPI api = WLServerAPIProvider.getWLServerAPI();
+	protected static final String CLOUDANT_DB = "adapter";
 
-	protected static final String CLOUDANT_DB = "test";
-
-	private static CloudantClient cloudant;
 	private static Database db;
+	private static String cloudantAccount = "";
+	private static String cloudantKey = "";
+	private static String cloudantPassword = "";
 
-	public static void init() {
-		String cloudantDomain = WorklightConfiguration.getInstance().getStringProperty("cloudant.domain");
-		String cloudantKey = WorklightConfiguration.getInstance().getStringProperty("cloudant.key");
-		String cloudantPassword = WorklightConfiguration.getInstance().getStringProperty("cloudant.password");
-		logger.info("cloudantDomain" + cloudantDomain);
+
+	@Context
+	ConfigurationAPI configurationAPI;
+
+
+
+	public Database getDB() {
+		if (updatedProperties() || db == null) {
+			//cloudant = new CloudantClient(cloudantDomain,cloudantKey,cloudantPassword);
+			CloudantClient cloudant = ClientBuilder.account(cloudantAccount)
+					.username(cloudantKey)
+					.password(cloudantPassword)
+					.build();
+			db = cloudant.database(CLOUDANT_DB, false);
+		}
+		return db;
+	}
+
+	private boolean updatedProperties() {
+		String cloudantAccountOld = cloudantAccount;
+		String cloudantKeyOld = cloudantKey;
+		String cloudantPasswordOld  = cloudantPassword;
+
+		cloudantAccount = configurationAPI.getPropertyValue("account");
+		cloudantKey = configurationAPI.getPropertyValue("key");
+		cloudantPassword = configurationAPI.getPropertyValue("password");
+
+		logger.info("cloudantDomain" + cloudantAccount);
 		logger.info("cloudantKey" + cloudantKey);
 		logger.info("cloudantPassword" + cloudantPassword);
 
-		cloudant = new CloudantClient(cloudantDomain,cloudantKey,cloudantPassword);
-		db = cloudant.database(CLOUDANT_DB, false);
+		return !cloudantAccountOld.equals(cloudantAccount) ||
+				!cloudantKeyOld.equals(cloudantKey) ||
+				!cloudantPasswordOld.equals(cloudantPassword);
 	}
+
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response addEntry(User user){
 		if(user!=null && user.isValid()){
-			db.save(user);
+			getDB().save(user);
 			return Response.ok().build();
 		}
 		else{
@@ -83,7 +109,7 @@ public class CloudantJavaResource {
 	public Response deleteEntry(@PathParam("id") String id, User user){
 		if(user!=null && user.get_id()!=null && user.get_rev()!=null && id.equals(user.get_id())){
 			try{
-				db.remove(user);
+				getDB().remove(user);
 				return Response.ok().build();
 			}
 			catch(NoDocumentException e){
@@ -98,8 +124,9 @@ public class CloudantJavaResource {
 
 	@GET
 	@Produces("application/json")
-	public Response getAllEntries(){
-		List<User> entries = db.view("_all_docs").includeDocs(true).query(User.class);
+	public Response getAllEntries() throws IOException {
+		//List<User> entries = db.view("_all_docs").includeDocs(true).query(User.class);
+		List<User> entries = getDB().getAllDocsRequestBuilder().includeDocs(true).build().getResponse().getDocsAs(User.class);
 		return Response.ok(entries).build();
 	}
 }
